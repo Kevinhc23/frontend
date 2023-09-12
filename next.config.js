f02cdb06d6837bad3a03494becc4039228f6ee1a
Kevin <kevinhernandezcrespo97@gmail.com>
2023-09-11 23:21:36 -0500

    custom output

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: {
+    publicPath: "dist/",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
