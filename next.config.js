df015135bdd0f0b9520f8196ab23befb435af1a1
Kevin <kevinhernandezcrespo97@gmail.com>
2023-09-12 00:13:32 -0500

    fix bug

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: {
-    publicPath: "dist/",
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -61,3 +61,4 @@ const handler = nextAuth({
 });
 
 export { handler as GET, handler as POST };
+export const runtime = "edge";
